eba99c3aaecdc2098c555aab8773d0375563de0a
Nino <nino.skopac@gmail.com>
2019-02-15 03:14:01 +0000

    Added a PHPDoc

--- array_column_recursive.php
+++ array_column_recursive.php
@@ -1,7 +1,14 @@
 <?php
+/**
+ * Returns values for $needle key in a multidimensional array, recursively.
+ * More info and example: https://github.com/NinoSkopac/array_column_recursive
+ *
+ * @param array $haystack
+ * @param string $needle
+ * @return array
+ */
 function array_column_recursive(array $haystack, $needle) {
     $found = [];
-
     array_walk_recursive($haystack, function($value, $key) use (&$found, $needle) {
         if ($key == $needle)
             $found[] = $value;
